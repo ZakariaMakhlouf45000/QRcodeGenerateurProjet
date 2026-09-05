5371af4707118ebd76a331a1ddfd44874a30994b
ZakariaMakhlouf45000 <zakaria.makhlouf45000@gmail.com>
2026-09-05 12:42:58 +0200

    first commit

---
+++ lecon1.js
@@ -0,0 +1,32 @@
+// const monBouton = document.getElementById('btn-magique');
+// monBouton.addEventListener('click', ()=> {               
+                                                         
+//     console.log("c'est cliquer");                        
+// });                                                      
+                                                         
+                                                         
+                                                         
+//          console.log("c'est cliquer");                                                
+                                                     
+// const Qrcode = require('qrcode');
+// const lien = "https://google.com";
+
+// Qrcode.toString(lien, {type:'terminal'}, (err,codeGene)=> {
+//      console.log(codeGene)
+// }
+// );
+
+const conteneur = document.getElementById('zone-qrcode');
+const monBouton = document.getElementById('btn-magique');
+
+const monInput = document.getElementById('texte-utilisateur');
+
+monBouton.addEventListener('click', () => {
+  const texteSaisi = monInput.value;
+  QRCode.toDataURL(texteSaisi, (err, url) => {
+    if (err) return;
+
+    console.log(url);
+    conteneur.innerHTML = `<img src="${url}">`;
+  });
+});
